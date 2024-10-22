a6b8481a2aee06ef76913eff3e7fd6271e1bdd7b
ivaaan <171681721+ivaaane@users.noreply.github.com>
2024-10-22 16:55:38 +0200

    changes to output.rs

--- src/output.rs
+++ src/output.rs
@@ -1,24 +1,32 @@
 use crate::word::Word;
 use colored::*;
 
+pub fn format_output(entry: &str, data: String) {
+    if data.len() > 0 {
+        println!("  {}\n    {}\n", entry.bold(), data);
+    }
+}
+
 pub fn output_data(word: &Word) {
     println!("{}\n", word.word.bold()); 
-    println!("{}\n    {} ({})\n", "usage".bold(), word.usage, word.book);
-    println!("{}\n    {}\n", "definition".bold(), word.definition);
+    format_output("usage", format!("{} ({})", word.usage, word.book));
+    format_output("definition", word.definition.to_string());
 
     let etymology: Vec<String> = word.etymology.iter()
         .map(|(_, value)| value.to_string())
         .collect();
-    println!("{}\n    {}: {}\n", "etymology".bold(), word.source_lang, etymology.join("; "));
+    format_output("etymology", format!("{}: {}", word.source_lang, etymology.join("; ")));
 
     let ku_data: Vec<String> = word.ku_data.iter()
         .map(|(key, value)| format!("{}: {}%", key, value))
         .collect();
-    println!("{}\n    {}\n", "ku data".bold(), ku_data.join(", "));
+    format_output("ku data", ku_data.join(", "));
 
     let see_also: Vec<String> = word.see_also.iter()
         .filter_map(|value| value.as_str())
         .map(|s| s.to_string())
         .collect();
-    println!("{}\n    {}", "see also".bold(), see_also.join(", "));
+    format_output("see also", see_also.join(", "));
+
+    println!("{}{}", "https://linku.la/words/".bright_black(), word.word.bright_black());
 }
